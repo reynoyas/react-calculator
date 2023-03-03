4304b86300b4a3d7b3d4d165774640168701412e
reynoyas <yasilizbeth@gmail.com>
2023-03-02 19:35:56 -0800

    Refactored code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Keypad from './Components/Keypad';
 
 function App() {
+  const [input, setInput] = useState('');
+  const handleClick = (value) => {
+    setInput(input + value);
+  };
+
+  const handleEqual = () => {
+    setInput(eval(input).toString());
+  };
+
+  const handleClear = () => {
+    setInput("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Calculator">
+        <input src={logo} className="display" alt="text" value={input} readOnly />
+        <Keypad
+          handleClear={handleClear}
+          handleClick={handleClick}
+          handleEqual={handleEqual}
+        />
+      </div>
     </div>
   );
 }
