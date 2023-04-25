b131b7086fe9da19795751b352167408d011560e
reynoyas <yasilizbeth@gmail.com>
2023-04-25 10:05:05 -0700

    Refactored operator keys

--- src/Components/Keypad.jsx
+++ src/Components/Keypad.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Keypad = ({ handleClick, handleClear, handleEqual }) => {
-    const nums = ["7", "8", "9", "4", "5", "6", "1", "2", "3"];
+    const nums = ["7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-"];
     const operations =  ["/", "*", "-", "+"];
 
     return (
@@ -9,19 +9,12 @@ const Keypad = ({ handleClick, handleClear, handleEqual }) => {
             {/* Iterate over nums array and render buttons for each number */}
             {nums.map((num, index) => (
                 <React.Fragment key={index}>
-                    <button className="calc-keys" onClick={() => handleClick(num)}>{num}</button>
+                    {/* Every 4th button has a className of operator-keys */}
+                    <button className={`calc-keys ${((index + 1) % 4 === 0) ? "operator-keys" : ""}`}
+                    onClick={() => handleClick(num)}>{num}</button>
                     
                     {/* Add line break after every 3 buttons */}
-                    {((index + 1) % 3 === 0) && <br />}
-                </React.Fragment>
-            ))}
-
-            {operations.map((operation, index) => (
-                <React.Fragment key={index}>
-                    <button className="operator-keys" onClick={() => handleClick(operation)}>{operation}</button>
-                    
-                    {/* Add line break after every 3 buttons */}
-                    {/* {((index + 1) % 3 === 0) && <br />} */}
+                    {((index + 1) % 4 === 0) && <br />}
                 </React.Fragment>
             ))}
         </div>
