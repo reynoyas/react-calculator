9f8db908582cf61e150c77cdcac72b1403207947
reynoyas <yasilizbeth@gmail.com>
2023-04-24 21:18:34 -0700

    Operator keys added

--- src/Components/Keypad.jsx
+++ src/Components/Keypad.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 
 const Keypad = ({ handleClick, handleClear, handleEqual }) => {
-    const nums = ["7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-"]
-    
+    const nums = ["7", "8", "9", "4", "5", "6", "1", "2", "3"];
+    const operations =  ["/", "*", "-", "+"];
+
     return (
         <div className="keypad">
             {/* Iterate over nums array and render buttons for each number */}
@@ -10,8 +11,17 @@ const Keypad = ({ handleClick, handleClear, handleEqual }) => {
                 <React.Fragment key={index}>
                     <button className="calc-keys" onClick={() => handleClick(num)}>{num}</button>
                     
-                    {/* Add line break after every 4 buttons */}
-                    {((index + 1) % 4 === 0) && <br />}
+                    {/* Add line break after every 3 buttons */}
+                    {((index + 1) % 3 === 0) && <br />}
+                </React.Fragment>
+            ))}
+
+            {operations.map((operation, index) => (
+                <React.Fragment key={index}>
+                    <button className="operator-keys" onClick={() => handleClick(operation)}>{operation}</button>
+                    
+                    {/* Add line break after every 3 buttons */}
+                    {/* {((index + 1) % 3 === 0) && <br />} */}
                 </React.Fragment>
             ))}
         </div>
